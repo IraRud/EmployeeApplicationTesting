fd073d0a833c0e65832596402c24023c3abf2994
rudskaya irina <irarudskaya5@gmail.com>
2023-10-18 09:52:33 +0300

    Initial commit

---
+++ EmployeeApplicationTesting/src/main/java/ru/skypro/lesson/EmployeeApplicationTesting/EmployeeApplicationTestingApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.lesson.EmployeeApplicationTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApplicationTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeApplicationTestingApplication.class, args);
+	}
+
+}
---
+++ EmployeeApplicationTesting/src/test/java/ru/skypro/lesson/EmployeeApplicationTesting/EmployeeApplicationTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.lesson.EmployeeApplicationTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
